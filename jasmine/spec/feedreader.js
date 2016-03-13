629a5d29ecc39a1ddb98351c32182eebfcf41bfa
Blake Gould <gouldbf@gmail.com>
2016-03-13 15:52:33 -0600

    Feat: Define a new test suite called "The Menu".

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -79,16 +79,19 @@ $(function() {
     });
 
 
-
-
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         it ('has a .entry element within the .feed container', function () {
+
+         });
+
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection"
 
